4236b3af397bce95be3a60af715a7d5ea45612c8
ChevalierSoft <ibanarashi@outlook.fr>
2020-07-15 08:05:03 +0200

    norming

--- color.h
+++ color.h
@@ -1,12 +1,26 @@
-# ifndef H_COLOR
-#define H_COLOR
-#define BLK   "\x1B[30m"
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RST   "\x1B[0m"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dait-atm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 22:21:58 by dait-atm          #+#    #+#             */
+/*   Updated: 2020/07/14 22:22:02 by dait-atm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_H
+
+# define COLOR_H
+# define BLK "\x1B[30m"
+# define RED "\x1B[31m"
+# define GRN "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHT "\x1B[37m"
+# define RST "\x1B[0m"
+
 #endif
